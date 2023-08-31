569426b67e3966e01eaa3da4978010a88695ff0c
Andrey-rab <andreyraaab@gmail.com>
2023-08-31 14:51:25 +0300

    Создал сортировку пузырьком

--- bubble_sorting.py
+++ bubble_sorting.py
@@ -1,8 +1,17 @@
 # Сортировка пузырьком
 
-start_list = [1, 2, 4, 42, 31, 24, 6, 7, 2, 3, 4, 5, 10]
+start_list = [5, 4, 3, 2, 1, 24, 6, 4, 5, 10, 7]
 # start_list.sort()
 # print(start_list)
 
-start_list = sorted(start_list)
-print(start_list)
+# start_list = sorted(start_list)
+# print(start_list)
+
+for item in range(len(start_list)-1):
+    for j in range(len(start_list)-1-item):
+        if start_list[j] > start_list[j + 1]:
+            temp = start_list[j]
+            start_list[j] = start_list[j + 1]
+            start_list[j + 1] = temp
+        print(start_list)
+
