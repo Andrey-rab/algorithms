5a3890dce424430c5a4ea84008bb220eb1528067
Andrey-rab <andreyraaab@gmail.com>
2023-09-02 21:34:34 +0300

    Написал линейный поиск

--- bubble_sorting.py
+++ bubble_sorting.py
@@ -10,8 +10,6 @@ start_list = [5, 4, 3, 2, 1, 24, 6, 4, 5, 10, 7]
 for item in range(len(start_list)-1):
     for j in range(len(start_list)-1-item):
         if start_list[j] > start_list[j + 1]:
-            temp = start_list[j]
-            start_list[j] = start_list[j + 1]
-            start_list[j + 1] = temp
-        print(start_list)
+            start_list[j], start_list[j + 1] = start_list[j + 1], start_list[j]
+print(start_list)
 
